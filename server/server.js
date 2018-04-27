2348894bc196fe1f2839783a12224df1fd45ea6a
TheCodeSleyer <ofir3322@gmail.com>
2018-04-27 18:38:21 +0300

    cors error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // enable Cross-Origin Request [IMPORTANT]
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100, *');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With , content-type , x-auth , x-provider');
     res.setHeader('Access-Control-Request-Headers', 'x-auth , x-provider')
