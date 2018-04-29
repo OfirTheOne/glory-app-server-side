c84a314c83d1c45c462e079adf7fcfd891be6fd1
TheCodeSleyer <ofir3322@gmail.com>
2018-04-29 11:07:51 +0300

    auth bugs

--- server/server.js
+++ server/server.js
@@ -11,9 +11,22 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
+app.use(function(req, res, next){
+    var whitelist = ['localhost:8100', 'guarded-escarpment-70218.herokuapp.com']
+    var host = req.get('host');
+  
+    whitelist.forEach(function(val, key){
+      if (host.indexOf(val) > -1){
+        res.setHeader('Access-Control-Allow-Origin', host);
+      }
+    })
+  
+    next();
+  });
+
 // enable Cross-Origin Request [IMPORTANT]
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With , content-type , x-auth , x-provider');
     res.setHeader('Access-Control-Request-Headers', 'x-auth , x-provider')
