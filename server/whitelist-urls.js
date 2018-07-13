b1e29fab0cf87cc48b97975aa0a8f358842958a5
TheCodeSleyer <ofir3322@gmail.com>
2018-07-13 18:36:04 +0300

    fix whitelist

--- server/whitelist-urls.js
+++ server/whitelist-urls.js
@@ -1,4 +1,4 @@
 
-const whitelist = ['http://localhost:8100', 'https://still-brushlands-49077.herokuapp.com/', 'https://guarded-escarpment-70218.herokuapp.com', 'localhost:8100'];
+const whitelist = ['http://localhost:8100', 'https://still-brushlands-49077.herokuapp.com', 'https://guarded-escarpment-70218.herokuapp.com', 'localhost:8100'];
 
 module.exports = whitelist;
